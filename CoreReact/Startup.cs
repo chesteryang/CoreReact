9a7b76c07cb035eb89b4670ee8d655261fad3165
Chester Yang <ChesterInShaw@gmail.com>
2020-06-02 15:15:47 -0600

    Move posts api to server side, change news to CA from US

--- CoreReact/Startup.cs
+++ CoreReact/Startup.cs
@@ -4,7 +4,6 @@ using CoreReact.Chinook.model;
 using CoreReact.Northwind.model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
